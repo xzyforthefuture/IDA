99bcc8640ca064c9c337bc3620ed6499a22a8c19
xzy <146616039+xzyforthefuture@users.noreply.github.com>
2026-03-14 16:00:02 +0800

    Add files via upload

---
+++ train/dataloader.py
@@ -0,0 +1,298 @@
+# -*- coding: utf-8 -*-
+"""
+@Project ：rec_torch 
+@File    ：dataLoader.py
+@Author  ：Knkiss
+@Date    ：2023/2/14 10:13 
+"""
+import collections
+import os
+import random
+from os.path import join
+from time import time
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from scipy.sparse import csr_matrix, coo_matrix
+
+import scipy.sparse as sp
+
+from train import utils
+import world
+
+
+class UIDataset(Dataset):
+    def __init__(self, path=None):
+        if path is None:
+            path = join(world.PATH_DATA, world.dataset)
+        print(f'loading [{path}]')
+        self.n_user = 0
+        self.m_item = 0
+        train_file = path + '/train.txt'
+        valid_file = path + '/valid.txt'
+        test_file = path + '/test.txt'
+        self.path = path
+        trainUniqueUsers, trainItem, trainUser = [], [], []
+        testUniqueUsers, testItem, testUser = [], [], []
+        validUniqueUsers, validItem, validUser = [], [], []
+        self.traindataSize = 0
+        self.testDataSize = 0
+        self.validDataSize = 0
+
+        with open(train_file) as f:
+            for line in f.readlines():
+                if len(line) > 0:
+                    line = line.strip('\n').split(' ')
+                    items = [int(i) for i in line[1:]]
+                    uid = int(line[0])
+                    trainUniqueUsers.append(uid)
+                    trainUser.extend([uid] * len(items))
+                    trainItem.extend(items)
+                    self.m_item = max(self.m_item, max(items, default=0))
+                    self.n_user = max(self.n_user, uid)
+                    self.traindataSize += len(items)
+        self.trainUniqueUsers = np.array(trainUniqueUsers)
+        self.trainUser = np.array(trainUser)
+        self.trainItem = np.array(trainItem)
+
+        with open(test_file) as f:
+            for line in f.readlines():
+                if len(line) > 0:
+                    line = line.strip('\n').split(' ')
+                    if line[1]:
+                        items = [int(i) for i in line[1:]]
+                        uid = int(line[0])
+                        testUniqueUsers.append(uid)
+                        testUser.extend([uid] * len(items))
+                        testItem.extend(items)
+                        self.m_item = max(self.m_item, max(items))
+                        self.n_user = max(self.n_user, uid)
+                        self.testDataSize += len(items)
+        self.testUniqueUsers = np.array(testUniqueUsers)
+        self.testUser = np.array(testUser)
+        self.testItem = np.array(testItem)
+
+        if os.path.exists(valid_file):
+            with open(valid_file) as f:
+                for line in f.readlines():
+                    if len(line) > 0:
+                        line = line.strip('\n').split(' ')
+                        if line[1]:
+                            items = [int(i) for i in line[1:]]
+                            uid = int(line[0])
+                            validUniqueUsers.append(uid)
+                            validUser.extend([uid] * len(items))
+                            validItem.extend(items)
+                            self.m_item = max(self.m_item, max(items))
+                            self.n_user = max(self.n_user, uid)
+                            self.validDataSize += len(items)
+            self.validUniqueUsers = np.array(validUniqueUsers)
+            self.validUser = np.array(validUser)
+            self.validItem = np.array(validItem)
+
+        self.m_item += 1
+        self.n_user += 1
+        self.Graph = None
+        print(f"{self.trainDataSize} interactions for training")
+        print(f"{self.testDataSize} interactions for testing")
+        print(f"{world.dataset} Sparsity : {(self.trainDataSize + self.testDataSize) / self.n_users / self.m_items}")
+
+        # (users,items), bipartite graph
+        self.UserItemNet = csr_matrix((np.ones(len(self.trainUser)), (self.trainUser, self.trainItem)),
+                                      shape=(self.n_user, self.m_item))
+        # pre-calculate
+        self._allPos = self.getUserPosItems(list(range(self.n_user)))
+        self.__testDict = self.__build_test()
+        print(f"{world.dataset} is ready to go")
+
+    @property
+    def n_users(self):
+        return self.n_user
+
+    @property
+    def m_items(self):
+        return self.m_item
+
+    @property
+    def trainDataSize(self):
+        return self.traindataSize
+
+    @property
+    def testDict(self):
+        return self.__testDict
+
+    @property
+    def allPos(self):
+        return self._allPos
+
+    def getSparseGraph(self):
+        print("loading adjacency matrix")
+        if self.Graph is None:
+            try:
+                pre_adj_mat = sp.load_npz(self.path + '/s_pre_adj_mat.npz')
+                print("successfully loaded...")
+                norm_adj = pre_adj_mat
+            except Exception:
+                print("generating adjacency matrix")
+                s = time()
+                adj_mat = sp.dok_matrix((self.n_users + self.m_items, self.n_users + self.m_items), dtype=np.float32)
+                adj_mat = adj_mat.tolil()
+                R = self.UserItemNet.tolil()
+                adj_mat[:self.n_users, self.n_users:] = R
+                adj_mat[self.n_users:, :self.n_users] = R.T
+                adj_mat = adj_mat.todok()
+                # adj_mat = adj_mat + sp.eye(adj_mat.shape[0])
+
+                rowsum = np.array(adj_mat.sum(axis=1))
+                d_inv = np.power(rowsum, -0.5).flatten()
+                d_inv[np.isinf(d_inv)] = 0.
+                d_mat = sp.diags(d_inv)
+
+                norm_adj = d_mat.dot(adj_mat)
+                norm_adj = norm_adj.dot(d_mat)
+                norm_adj = norm_adj.tocsr()
+                end = time()
+                print(f"costing {end - s}s, saved norm_mat...")
+                sp.save_npz(self.path + '/s_pre_adj_mat.npz', norm_adj)
+            self.Graph = utils.convert_sp_mat_to_sp_tensor(norm_adj)
+            self.Graph = self.Graph.coalesce().to(world.device)
+        return self.Graph
+
+    def __build_test(self):
+        test_data = {}
+        for i, item in enumerate(self.testItem):
+            user = self.testUser[i]
+            if test_data.get(user):
+                test_data[user].append(item)
+            else:
+                test_data[user] = [item]
+        return test_data
+
+    def getUserItemFeedback(self, users, items):
+        return np.array(self.UserItemNet[users, items]).astype('uint8').reshape((-1,))
+
+    def getUserPosItems(self, users):
+        posItems = []
+        for user in users:
+            posItems.append(self.UserItemNet[user].nonzero()[1])
+        return posItems
+
+    def __len__(self):
+        return self.traindataSize
+
+    def __getitem__(self, idx):
+        user = self.trainUser[idx]
+        pos = random.choice(self._allPos[user])
+        while True:
+            neg = np.random.randint(0, self.m_item)
+            if neg in self._allPos[user]:
+                continue
+            else:
+                break
+        return user, pos, neg
+
+
+class KGDataset(Dataset):
+    def __init__(self, kg_path=None):
+        if kg_path is None:
+            kg_path = join(world.PATH_DATA, world.dataset, "kg.txt")
+        kg_data = pd.read_csv(kg_path, sep=' ', names=['h', 'r', 't'], engine='python')
+        self.kg_data = kg_data.drop_duplicates()
+        self.kg_dict, self.heads = self.generate_kg_data()
+        self.item_net_path = join(world.PATH_DATA, world.dataset)
+        self.length = len(self.kg_dict)
+
+    @property
+    def entity_count(self):
+        return self.kg_data['t'].max() + 2
+
+    @property
+    def relation_count(self):
+        return self.kg_data['r'].max() + 2
+
+    def get_kg_graph(self):
+        head = self.kg_data['h'].values
+        tail = self.kg_data['t'].values + head.max()
+        value = self.kg_data['r'].values
+        kg_graph = coo_matrix((value, (head, tail)), shape=(tail.max() + 1, tail.max() + 1))
+        return kg_graph
+
+    def get_kg_dict(self, item_num):
+        entity_num = world.hyper_KGDataset_entity_num_per_item
+        i2es = dict()
+        i2rs = dict()
+        for item in range(item_num):
+            rts = self.kg_dict.get(item, False)
+            if rts:
+                tails = list(map(lambda x: x[1], rts))
+                relations = list(map(lambda x: x[0], rts))
+                if len(tails) > entity_num:
+                    i2es[item] = torch.IntTensor(tails).to(world.device)[:entity_num]
+                    i2rs[item] = torch.IntTensor(relations).to(world.device)[:entity_num]
+                else:
+                    # last embedding pos as padding idx
+                    tails.extend([self.entity_count] * (entity_num - len(tails)))
+                    relations.extend([self.relation_count] * (entity_num - len(relations)))
+                    i2es[item] = torch.IntTensor(tails).to(world.device)
+                    i2rs[item] = torch.IntTensor(relations).to(world.device)
+            else:
+                i2es[item] = torch.IntTensor([self.entity_count] * entity_num).to(world.device)
+                i2rs[item] = torch.IntTensor([self.relation_count] * entity_num).to(world.device)
+        return i2es, i2rs
+
+    def generate_kg_data(self):
+        kg_dict = collections.defaultdict(list)
+
+        # DIFF 性能未知 部分relation对性能有一定提升
+        relation_list = 'All'
+        for row in self.kg_data.iterrows():
+            h, r, t = row[1]
+            if relation_list == 'All' or r in relation_list:
+                kg_dict[h].append((r, t))
+        heads = list(kg_dict.keys())
+        return kg_dict, heads
+
+    def get_kg_dict_random(self, item_num):
+        kg_dict = collections.defaultdict(list)
+        a = self.kg_data.sample(frac=0.8)
+        for row in a.iterrows():
+            h, r, t = row[1]
+            kg_dict[h].append((r, t))
+
+        entity_num = world.hyper_KGDataset_entity_num_per_item
+        i2es = dict()
+        i2rs = dict()
+        for item in range(item_num):
+            rts = kg_dict.get(item, False)
+            if rts:
+                tails = list(map(lambda x: x[1], rts))
+                relations = list(map(lambda x: x[0], rts))
+                if len(tails) > entity_num:
+                    i2es[item] = torch.IntTensor(tails).to(world.device)[:entity_num]
+                    i2rs[item] = torch.IntTensor(relations).to(world.device)[:entity_num]
+                else:
+                    tails.extend([self.entity_count] * (entity_num - len(tails)))
+                    relations.extend([self.relation_count] * (entity_num - len(relations)))
+                    i2es[item] = torch.IntTensor(tails).to(world.device)
+                    i2rs[item] = torch.IntTensor(relations).to(world.device)
+            else:
+                i2es[item] = torch.IntTensor([self.entity_count] * entity_num).to(world.device)
+                i2rs[item] = torch.IntTensor([self.relation_count] * entity_num).to(world.device)
+        return i2es, i2rs
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        head = self.heads[index]
+        relation, pos_tail = random.choice(self.kg_dict[head])
+        while True:
+            neg_head = random.choice(self.heads)
+            neg_tail = random.choice(self.kg_dict[neg_head])[1]
+            if (relation, neg_tail) in self.kg_dict[head]:
+                continue
+            else:
+                break
+        return head, relation, pos_tail, neg_tail
---
+++ train/losses.py
@@ -0,0 +1,231 @@
+from enum import Enum
+
+import torch
+
+from train import utils
+import world
+import torch.nn.functional as F
+
+
+class Loss(Enum):
+    BPR = 'BPR'
+    SSL = 'SSL'
+    TransE = 'TransE'
+    Regulation = 'reg'
+
+
+def loss_BPR(all_users, all_items, users, pos, neg):
+    users_emb = all_users[users.long()]
+    pos_emb = all_items[pos.long()]
+    neg_emb = all_items[neg.long()]
+    pos_scores = torch.mul(users_emb, pos_emb).sum(dim=1)
+    neg_scores = torch.mul(users_emb, neg_emb).sum(dim=1)
+    loss = torch.sum(torch.nn.functional.softplus(-(pos_scores - neg_scores)))  # mean or sum
+    return loss
+
+
+def loss_BPR_weighted(all_users, all_items, u1, u2, i1, i2, users, pos, neg):
+    # 使用知识个性化权重的BPR
+    users_emb = all_users[users.long()]
+    pos_emb = all_items[pos.long()]
+    neg_emb = all_items[neg.long()]
+    pos_scores = torch.mul(users_emb, pos_emb).sum(dim=1)
+    neg_scores = torch.mul(users_emb, neg_emb).sum(dim=1)
+    scores = -(pos_scores - neg_scores)
+
+    u1_emb = u1[users.long()]
+    u2_emb = u2[users.long()]
+    i1_emb = i1[pos.long()]
+    i2_emb = i2[pos.long()]
+
+    inter_1 = torch.mul(u1_emb, i1_emb)
+    inter_2 = torch.mul(u2_emb, i2_emb)
+    sim = F.cosine_similarity(inter_1, inter_2)  # inters
+    sim = (sim - sim.min()) / (sim.max() - sim.min())
+    # sim = torch.exp(sim)
+    # item_stabilities = t
+    # orch.exp(item_stabilities)
+
+    logits = sim.detach()
+
+    loss = torch.sum(torch.nn.functional.softplus(scores * logits))
+    return loss
+
+
+def loss_SSM_origin(all_users, all_items, users, pos):
+    batch_user_emb = F.normalize(all_users[users.long()], dim=1)
+    batch_item_emb = F.normalize(all_items[pos.long()], dim=1)
+    pos_score = torch.sum(torch.multiply(batch_user_emb, batch_item_emb), dim=1, keepdim=True)
+    ttl_score = torch.matmul(batch_user_emb, batch_item_emb.T) * world.hyper_SSM_Margin
+    logits = ttl_score - pos_score
+    clogits = torch.logsumexp(logits / world.hyper_SSM_Loss_temp, dim=1)
+    loss = torch.sum(clogits)
+    return loss
+
+
+def loss_New_1_1(all_users, all_items, users, pos, neg):
+    users_emb = all_users[users.long()]
+    pos_emb = all_items[pos.long()]
+    neg_emb = all_items[neg.long()]
+
+    pos_scores = torch.mul(users_emb, pos_emb).sum(dim=1)
+
+    neg_scores = torch.matmul(users_emb, neg_emb.T)
+    neg_scores = torch.mean(neg_scores, dim=1)
+
+    loss = torch.sum(torch.nn.functional.softplus(-(pos_scores - neg_scores)))
+    return loss
+
+
+def loss_New_1_2(all_users, all_items, users, pos, neg):
+    users_emb = all_users[users.long()]
+    pos_emb = all_items[pos.long()]
+    neg_emb = all_items[neg.long()]
+
+    pos_scores = torch.mul(users_emb, pos_emb).sum(dim=1)
+
+    neg_scores = torch.matmul(users_emb, neg_emb.T)
+    neg_scores = torch.exp(neg_scores)
+    neg_scores = torch.mean(neg_scores, dim=1)
+    neg_scores = torch.log(neg_scores)
+
+    loss = torch.sum(torch.nn.functional.softplus(-(pos_scores - neg_scores)))
+    return loss
+
+
+def loss_New_1_3(all_users, all_items, users, pos, neg):
+    users_emb = all_users[users.long()]
+    pos_emb = all_items[pos.long()]
+    neg_emb = all_items[neg.long()]
+
+    pos_scores = torch.mul(users_emb, pos_emb).sum(dim=1)
+
+    neg_scores_1 = torch.mul(users_emb, neg_emb).sum(dim=1)
+
+    neg_scores = torch.matmul(users_emb, pos_emb.T)
+    neg_scores = torch.exp(neg_scores)
+    neg_scores = torch.mean(neg_scores, dim=1)
+    neg_scores = torch.log(neg_scores)
+
+    loss = torch.sum(torch.nn.functional.softplus(-(pos_scores - neg_scores_1 - neg_scores)))
+    return loss
+
+
+def loss_New_2_1(all_users, all_items, users, pos, neg):
+    batch_user_emb = F.normalize(all_users[users.long()], dim=1)
+    batch_item_emb = F.normalize(all_items[pos.long()], dim=1)
+    batch_item_emb_neg = F.normalize(all_items[neg.long()], dim=1)
+
+    pos = torch.mul(batch_user_emb, batch_item_emb).sum(dim=1)
+    neg = torch.mul(batch_user_emb, batch_item_emb_neg).sum(dim=1)
+    pos_score = torch.nn.functional.softplus(pos - neg).unsqueeze(dim=1)
+
+    ttl_score_neg_all = torch.matmul(batch_user_emb, batch_item_emb.T) * world.hyper_SSM_Margin
+    ttl_score_neg = torch.matmul(batch_user_emb, batch_item_emb_neg.T) * world.hyper_SSM_Margin
+    ttl_score = ttl_score_neg_all + ttl_score_neg
+
+    logits = ttl_score - pos_score
+    clogits = torch.logsumexp(logits / world.hyper_SSM_Loss_temp, dim=1)
+    loss = torch.sum(clogits)
+    return loss
+
+
+def loss_New_2_2(all_users, all_items, users, pos, neg):
+    batch_user_emb = F.normalize(all_users[users.long()], dim=1)
+    batch_item_emb = F.normalize(all_items[pos.long()], dim=1)
+    batch_neg_emb = F.normalize(all_items[neg.long()], dim=1)
+
+    pos_score = torch.sum(torch.multiply(batch_user_emb, batch_item_emb), dim=1)
+    pos_score = torch.exp(pos_score / world.hyper_SSM_Loss_temp)
+
+    neg_score = torch.sum(torch.multiply(batch_user_emb, batch_neg_emb), dim=1)
+    neg_score = torch.exp(neg_score / world.hyper_SSM_Loss_temp)
+
+    ttl_score = torch.matmul(batch_user_emb, batch_item_emb.T)
+    ttl_score = torch.sum(torch.exp(ttl_score / world.hyper_SSM_Loss_temp), dim=1)
+
+    clogits = -(torch.log(pos_score) - torch.log(ttl_score * world.hyper_test_ratio + neg_score * world.hyper_test_ratio_2))
+    loss = torch.sum(clogits)
+    return loss
+
+
+def loss_New_2_3(all_users, all_items, users, pos, neg):
+    batch_user_emb = F.normalize(all_users[users.long()], dim=1)
+    batch_item_emb = F.normalize(all_items[pos.long()], dim=1)
+    batch_neg_emb = F.normalize(all_items[neg.long()], dim=1)
+
+    pos_score = torch.sum(torch.multiply(batch_user_emb, batch_item_emb), dim=1)
+    pos_score = torch.exp(pos_score / world.hyper_SSM_Loss_temp)
+
+    neg_score = torch.sum(torch.multiply(batch_user_emb, batch_neg_emb), dim=1)
+    neg_score = torch.exp(neg_score / world.hyper_SSM_Loss_temp)
+
+    ttl_score = torch.matmul(batch_user_emb, batch_item_emb.T)
+    ttl_score = torch.sum(torch.exp(ttl_score / world.hyper_SSM_Loss_temp), dim=1)
+
+    clogits = -(torch.log(pos_score) - torch.log(ttl_score * neg_score))
+    loss = torch.sum(clogits)
+    return loss
+
+
+def loss_regulation(all_users_origin, all_items_origin, users, pos, neg):
+    userEmb0 = all_users_origin(users.long())
+    posEmb0 = all_items_origin(pos.long())
+    negEmb0 = all_items_origin(neg.long())
+    loss = (1 / 2) * (userEmb0.norm(2).pow(2) + posEmb0.norm(2).pow(2) + negEmb0.norm(2).pow(2)) / float(len(users))
+    return loss * world.hyper_decay
+
+
+def loss_info_nce(node_v1, node_v2, batch):
+    # KGCL文章代码中的对比Loss计算形式
+    z1 = node_v1[batch]
+    z2 = node_v2[batch]
+    z_all = node_v2
+
+    def f(x):
+        return torch.exp(x / world.hyper_ssl_temp)
+
+    all_sim = f(utils.sim(z1, z_all))
+    positive_pairs = f(utils.sim(z1, z2))
+    negative_pairs = torch.sum(all_sim, 1)
+    loss = torch.sum(-torch.log(positive_pairs / negative_pairs))
+    return loss * world.hyper_ssl_reg
+
+
+def loss_SGL(node_v1, node_v2, batch):
+    # SGL文章代码中的对比Loss计算形式
+    emb1 = node_v1[batch]
+    emb2 = node_v2[batch]
+
+    normalize_emb1 = F.normalize(emb1, 1)
+    normalize_emb2 = F.normalize(emb2, 1)
+    normalize_all_emb2 = F.normalize(node_v2, dim=1)
+
+    pos_score = torch.sum(torch.mul(normalize_emb1, normalize_emb2), dim=1)
+    ttl_score = torch.matmul(normalize_emb1, normalize_all_emb2.T)
+
+    pos_score = torch.exp(pos_score / world.hyper_ssl_temp)
+    ttl_score = torch.sum(torch.exp(ttl_score / world.hyper_ssl_temp), dim=1)
+
+    loss = -torch.sum(torch.log(pos_score / ttl_score))
+    return loss * world.hyper_ssl_reg
+
+
+def loss_transE(head, tail, relation, h, r, pos_t, neg_t):
+    r_embed = relation(r)
+    h_embed = head(h)
+    pos_t_embed = tail(pos_t)
+    neg_t_embed = tail(neg_t)
+
+    pos_score = torch.sum(torch.pow(h_embed + r_embed - pos_t_embed, 2), dim=1)
+    neg_score = torch.sum(torch.pow(h_embed + r_embed - neg_t_embed, 2), dim=1)
+
+    kg_loss = (-1.0) * F.logsigmoid(neg_score - pos_score)
+    kg_loss = torch.mean(kg_loss)
+
+    def L2_loss_mean(x):
+        return torch.mean(torch.div(torch.sum(torch.pow(x, 2), dim=1, keepdim=False), 2.))
+
+    l2_loss = L2_loss_mean(h_embed) + L2_loss_mean(r_embed) + L2_loss_mean(pos_t_embed) + L2_loss_mean(neg_t_embed)
+    loss = kg_loss + 1e-3 * l2_loss
+    return loss
